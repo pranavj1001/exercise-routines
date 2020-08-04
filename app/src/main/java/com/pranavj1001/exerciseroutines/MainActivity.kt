e250b4f25a7bd29bebc43514c90456f3e696302c
pranavj1001 <pranavj1001@gmail.com>
2020-08-04 23:12:11 +0530

    add onclick and onlongpress events for routine list

--- app/src/main/java/com/pranavj1001/exerciseroutines/MainActivity.kt
+++ app/src/main/java/com/pranavj1001/exerciseroutines/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.pranavj1001.exerciseroutines
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnItemTouchListener
 import com.google.gson.Gson
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -33,8 +40,19 @@ class MainActivity : AppCompatActivity() {
 
             // specify an viewAdapter (see also next example)
             adapter = viewAdapter
-
         }
+
+        recyclerView.addOnItemTouchListener(
+            RecyclerItemClickListener(applicationContext, recyclerView, object : RecyclerItemClickListener.OnItemClickListener {
+                override fun onItemClick(view: View?, position: Int) {
+                    Toast.makeText(applicationContext, "clicked!", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onLongItemClick(view: View?, position: Int) {
+                    Toast.makeText(applicationContext, "long clicked!", Toast.LENGTH_SHORT).show()
+                }
+            })
+        )
     }
 
     /**
@@ -69,4 +87,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+}
+
+
+class RecyclerItemClickListener(
+    context: Context?,
+    recyclerView: RecyclerView,
+    private val mListener: OnItemClickListener?
+) :
+    OnItemTouchListener {
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View?, position: Int)
+        fun onLongItemClick(view: View?, position: Int)
+    }
+
+    var mGestureDetector: GestureDetector
+    override fun onInterceptTouchEvent(view: RecyclerView, e: MotionEvent): Boolean {
+        val childView = view.findChildViewUnder(e.x, e.y)
+        if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+            mListener.onItemClick(childView, view.getChildAdapterPosition(childView))
+            return true
+        }
+        return false
+    }
+
+    override fun onTouchEvent(view: RecyclerView, motionEvent: MotionEvent) {}
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+
+    init {
+        mGestureDetector = GestureDetector(context, object : SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                return true
+            }
+
+            override fun onLongPress(e: MotionEvent) {
+                val child = recyclerView.findChildViewUnder(e.x, e.y)
+                if (child != null && mListener != null) {
+                    mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child))
+                }
+            }
+        })
+    }
 }
